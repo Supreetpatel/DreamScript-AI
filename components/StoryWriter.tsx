062b62fa0b781739dcee7f6e70f646683b06fd85
Supreet0 <supreetpatel2004@gmail.com>
2025-05-30 19:59:19 +0530

    Made the home page for the story telling website

---
+++ components/StoryWriter.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import { useState } from "react";
+import { Button } from "./ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+import { Textarea } from "./ui/textarea";
+
+const StoryWriter = () => {
+  const [story, setStory] = useState<string>("");
+  const [pages, setPages] = useState<number>();
+  const [progress, setProgress] = useState("");
+  const [runStarted, setRunStarted] = useState<boolean>(false);
+  const [runFinished, setRunFinished] = useState<boolean | null>(null);
+  const [currentTool, setCurrentTool] = useState("");
+
+  return (
+    <div className="flex flex-col container">
+      <section className="flex-1 flex flex-col border-2  border-purple-300 rounded-md p-10 space-y-2">
+        <Textarea
+          value={story}
+          onChange={(e) => setStory(e.target.value)}
+          className="flex-1 text-black"
+          placeholder="Write a story about a robot and a human who become friends..."
+        />
+        <Select onValueChange={(value) => setPages(parseInt(value))}>
+          <SelectTrigger>
+            <SelectValue placeholder="How many pages should the story be?" />
+          </SelectTrigger>
+          <SelectContent className="w-full">
+            {Array.from({ length: 10 }, (_, i) => (
+              <SelectItem key={i} value={String(i + 1)}>
+                {i + 1} page{i === 0 ? "" : "s"}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+        <Button disabled={!story || !pages} className="w-full" size="lg">
+          Generate Story
+        </Button>
+      </section>
+      <section className="flex-1 pb-5 mt-5">
+        <div className="flex flex-col-reverse w-full space-y-2 bg-gray-800 text-gray-200 font-mono p-10 h-96 rounded-md overflow-y-auto">
+          <div>
+            {runFinished === null && (
+              <>
+                <p className="animate-pulse mr-5">
+                  I&apos;m waiting for you to generate a story above...
+                </p>
+                <br />
+              </>
+            )}
+            <span className="mr-5">{">>"}</span>
+            {progress}
+          </div>
+          {/**/}
+          {currentTool && (
+            <div className="py-10">
+              <span className="mr-5">{"--- [Current Tool] ---"}</span>
+            </div>
+          )}
+          {/*Render Events...*/}
+          {runStarted && (
+            <div>
+              <span className="mr-5 animate-in">
+                {"--- [AI Storyteller Has Started] ---"}
+              </span>
+              <br />
+            </div>
+          )}
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default StoryWriter;
